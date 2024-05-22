7de852774186ee4a42730ba0512784ea3dae0612
joseph uzuegbu <josephuzuegbu55@gmail.com>
2024-05-22 19:08:49 +0100

    fix

--- app/(client)/contact/page.tsx
+++ app/(client)/contact/page.tsx
@@ -1,4 +1,9 @@
 import Banner from "../generic/Banner";
+import { MdLocationPin } from "react-icons/md";
+import { IoIosCall } from "react-icons/io";
+import { MdAccessTimeFilled } from "react-icons/md";
+import Input, { Textarea } from "../generic/Input";
+import Button from "../generic/Button";
 
 export default function page() {
   const links = [
@@ -15,6 +20,77 @@ export default function page() {
           Drop Us An Email. Our Staff Always Be There To Help You Out. Do Not
           Hesitate!
         </p>
+      </div>
+      <div className="grid grid-cols-2 gap-10 w-[80%] mx-auto my-10">
+        {/* first div */}
+        <div className="flex flex-col space-y-4">
+          <div className="flex gap-3">
+            <MdLocationPin />
+            <div className="flex flex-col">
+              <h2 className="font-bold">Address</h2>
+              <p>236 5th SE Avenue, New </p>
+              <p>York NY10000, United </p>
+              <p>States.</p>
+            </div>
+          </div>
+
+          <div className="flex gap-3">
+            <IoIosCall />
+            <div className="flex flex-col">
+              <h2 className="font-bold">Address</h2>
+              <p>Mobile: +(84) 546-6789</p>
+              <p>Hotline: +(84) 456-6789</p>
+            </div>
+          </div>
+
+          <div className="flex gap-3">
+            <MdAccessTimeFilled />
+            <div className="flex flex-col">
+              <h2 className="font-bold">Working Time</h2>
+              <p>Monday-Friday: 9:00 - 22:00</p>
+              <p>Saturday-Sunday: 9:00 - 21:00</p>
+            </div>
+          </div>
+        </div>
+
+        {/* second div */}
+        <form method="post" className="flex flex-col  space-y-4">
+          <Input
+            type="text"
+            labelName="Your name"
+            errorLabel=""
+            className="border p-3 outline-none"
+            placeholder="Abc"
+          />
+
+          <Input
+            type="email"
+            labelName="Email address"
+            errorLabel=""
+            className="border p-3 outline-none"
+            placeholder="Abc@def.com"
+          />
+
+          <Input
+            type="text"
+            labelName="Subject"
+            errorLabel=""
+            className="border p-3 outline-none"
+            placeholder="This is an optional"
+          />
+
+          <Textarea
+            labelName="Message"
+            errorLabel=""
+            className="border p-3 outline-none"
+            placeholder="Hi! i’d like to ask about"
+          />
+
+          <Button
+            textContent="Submit"
+            className="bg-yellow-600 w-[200px] p-3 rounded-md text-white"
+          />
+        </form>
       </div>
     </section>
   );
--- app/(client)/generic/Input.tsx
+++ app/(client)/generic/Input.tsx
@@ -9,6 +9,12 @@ interface ISelectType extends React.InputHTMLAttributes<HTMLSelectElement> {
   data: any[];
 }
 
+interface ITextareaInput
+  extends React.InputHTMLAttributes<HTMLTextAreaElement> {
+  labelName: string;
+  errorLabel?: string;
+}
+
 export default function Input({ labelName, errorLabel, ...props }: InputType) {
   return (
     <section className="flex flex-col w-full">
@@ -34,6 +40,22 @@ export const SelectInput = ({
       <select {...props} name="" id="">
         <option value=""></option>
       </select>
+      <span className="text-xs">{errorLabel}</span>
+    </section>
+  );
+};
+
+export const Textarea = ({
+  labelName,
+  errorLabel,
+  ...props
+}: ITextareaInput) => {
+  return (
+    <section className="flex flex-col w-full">
+      <label htmlFor={labelName} className="mb-2 font-medium">
+        {labelName}
+      </label>
+      <textarea {...props}></textarea>
       <span className="text-xs">{errorLabel}</span>
     </section>
   );
