4f76a1a66e9353a3cb796ece857fd21a015da34b
josephDev123 <josephuzuegbu55@gmail.com>
2024-11-12 13:33:30 +0100

    fix UI of categoies

--- app/(dashboard)/categories/components/CategoryMainWrapper.tsx
+++ app/(dashboard)/categories/components/CategoryMainWrapper.tsx
@@ -41,8 +41,8 @@ export default function CategoryMainWrapper({
 
   // console.log(filteredData, searchCategory);
   return (
-    <section>
-      <div className="flex sm:flex-row flex-col justify-between items-start gap-4 my-4">
+    <section className="flex flex-col w-full">
+      <div className="flex sm:flex-row flex-col justify-between  items-start gap-4 my-4">
         {/* <div className="bg-red-200 w-full"> */}
         <Input
           name="categorySearch"
@@ -73,7 +73,7 @@ export default function CategoryMainWrapper({
 
           <span className="text-left px-4"></span>
         </div>
-        <div className="">
+        <div className="w-full">
           {filteredData.map((item, i) => (
             <>
               <div className="flex items-center w-full">
@@ -100,111 +100,143 @@ export default function CategoryMainWrapper({
                   />
                 </span>
               </div>
-            </>
-          ))}
-
-          <div className="overflow-x-auto flex flex-col w-full">
-            <div
-              className={`flex items-center w-full 
+              <div className="flex items-center">
+                {isDropdown.state && isDropdown.index === item._id && (
+                  <div className="overflow-x-auto flex w-full flex-col ">
+                    {/* <div
+                    className={`flex items-center w-full 
                 ${isDropdown.state ? "block" : "hidden"}   
                   `}
-            >
-              <span className="text-left px-4 flex-1 text-nowrap"> ID</span>
-              <span className="text-left px-4 flex-1 text-nowrap">
-                {" "}
-                PRODUCT NAME
-              </span>
-              <span className="text-left px-4 flex-1 text-nowrap">
-                PRODUCT DESCRIPTION
-              </span>
-              <span className="text-left px-4 flex-1 text-nowrap">
-                PRODUCT TAG
-              </span>
-              <span className="text-left px-4 flex-1 text-nowrap">
-                PRODUCT PRICE
-              </span>
-              <span className="text-left px-4 flex-1 text-nowrap">
-                PRODUCT DISCOUNT
-              </span>
-              <span className="text-left px-4 flex-1 text-nowrap">
-                PRODUCT QUANTITY
-              </span>
-              <span className="text-left px-4 flex-1 text-nowrap">
-                PRODUCT SKU
-              </span>
-              <span className="text-left px-4 flex-1 text-nowrap">
-                PRODUCT SIZE
-              </span>
-              <span className="text-left px-4 flex-1 text-nowrap">
-                PRODUCT WEIGHT
-              </span>
-              <span className="text-left px-4 flex-1 text-nowrap">
-                PRODUCT UNIT
-              </span>
-              <span className="text-left px-4 flex-1 text-nowrap">
-                PRODUCT BREATH
-              </span>
-              <span className="text-left px-4 flex-1 text-nowrap">
-                PRODUCT LENGTH
-              </span>
-              <span className="text-left px-4 flex-1 text-nowrap">
-                PRODUCT WIDTH
-              </span>
-              <span className="text-left px-4 flex-1 text-nowrap">
-                PRODUCT IMAGE
-              </span>
-              <span className="text-left px-4"></span>
-            </div>
-            {isDropdown.state && (
-              <>
-                {products.products.map((product, i) => (
-                  <div key={i} className="w-full border">
-                    <span className="p-2 flex-1 text-nowrap">{product.id}</span>
-                    <span className="p-2 flex-1 text-nowrap">
-                      {product.productName}
+                  >
+                    <span className="text-left px-4 flex-1 text-nowrap">
+                      {" "}
+                      ID
                     </span>
-                    <span className="p-2 flex-1 text-nowrap">
-                      {product.Description}
+                    <span className="text-left px-4 flex-1 text-nowrap">
+                      {" "}
+                      PRODUCT NAME
                     </span>
-                    <span className="p-2 flex-1 text-nowrap">
-                      {product.productTag}
+                    <span className="text-left px-4 flex-1 text-nowrap">
+                      PRODUCT DESCRIPTION
                     </span>
-                    <span className="p-2 flex-1 text-nowrap">
-                      {product.productPrice}
+                    <span className="text-left px-4 flex-1 text-nowrap">
+                      PRODUCT TAG
                     </span>
-                    <span className="p-2 flex-1 text-nowrap">
-                      {product.productDiscount}
+                    <span className="text-left px-4 flex-1 text-nowrap">
+                      PRODUCT PRICE
                     </span>
-                    <span className="p-2 flex-1 text-nowrap">
-                      {product.productQuantity}
+                    <span className="text-left px-4 flex-1 text-nowrap">
+                      PRODUCT DISCOUNT
                     </span>
-                    <span className="p-2 flex-1 text-nowrap">
-                      {product.productSKU}
+                    <span className="text-left px-4 flex-1 text-nowrap">
+                      PRODUCT QUANTITY
                     </span>
-                    <span className="p-2 flex-1 text-nowrap">
-                      {product.productSize}
+                    <span className="text-left px-4 flex-1 text-nowrap">
+                      PRODUCT SKU
                     </span>
-                    <span className="p-2 flex-1 text-nowrap">
-                      {product.productItemWeight}
+                    <span className="text-left px-4 flex-1 text-nowrap">
+                      PRODUCT SIZE
                     </span>
-                    <span className="p-2 flex-1 text-nowrap">
-                      {product.productUnit}
+                    <span className="text-left px-4 flex-1 text-nowrap">
+                      PRODUCT WEIGHT
                     </span>
-                    <span className="p-2 flex-1 text-nowrap">
-                      {product.productBreath}
+                    <span className="text-left px-4 flex-1 text-nowrap">
+                      PRODUCT UNIT
                     </span>
-                    <span className="p-2 flex-1 text-nowrap">
-                      {product.productLength}
+                    <span className="text-left px-4 flex-1 text-nowrap">
+                      PRODUCT BREATH
                     </span>
-                    <span className="p-2 flex-1 text-nowrap">
-                      {product.productWidth}
+                    <span className="text-left px-4 flex-1 text-nowrap">
+                      PRODUCT LENGTH
                     </span>
-                    <span className="p-2 text-nowrap flex-1">{product.id}</span>
+                    <span className="text-left px-4 flex-1 text-nowrap">
+                      PRODUCT WIDTH
+                    </span>
+                    <span className="text-left px-4 flex-1 text-nowrap">
+                      PRODUCT IMAGE
+                    </span>
+                    <span className="text-left px-4"></span>
+                  </div> */}
+
+                    <>
+                      {item.products.map((product, i) => (
+                        <div
+                          key={i}
+                          className={`w-full border h-60 overflow-y-auto  space-y-4 p-2 ${
+                            i + 1 / 2 === 0 ? "bg-yellow-100" : "bg-gray-200"
+                          }`}
+                        >
+                          <span className="flex flex-col text-nowrap">
+                            <h1 className="font-bold">No:</h1>
+                            {i + 1}
+                          </span>
+                          <span className=" flex flex-col text-nowrap">
+                            <h1 className="font-bold">Product Id:</h1>
+                            {product.id}
+                          </span>
+                          <span className=" flex flex-col text-nowrap">
+                            <h1 className="font-bold">Product name:</h1>
+                            {product.productName}
+                          </span>
+                          <span className=" flex flex-col text-nowrap">
+                            <h1 className="font-bold">Product description:</h1>
+                            {product.Description}
+                          </span>
+                          <span className=" flex flex-col text-nowrap">
+                            <h1 className="font-bold">Product ag:</h1>
+                            {product.productTag}
+                          </span>
+                          <span className=" flex flex-col text-nowrap">
+                            <h1 className="font-bold">Product price:</h1>
+                            {product.productPrice}
+                          </span>
+                          <span className=" flex flex-col text-nowrap">
+                            <h1 className="font-bold">Product discount:</h1>
+                            {product.productDiscount}
+                          </span>
+                          <span className=" flex flex-col text-nowrap">
+                            <h1 className="font-bold">Product qty:</h1>
+                            {product.productQuantity}
+                          </span>
+                          <span className=" flex flex-col text-nowrap">
+                            <h1 className="font-bold">Product SKU:</h1>
+                            {product.productSKU}
+                          </span>
+                          <span className=" flex flex-col text-nowrap">
+                            <h1 className="font-bold">Product size:</h1>
+                            {product.productSize}
+                          </span>
+                          <span className=" flex flex-col text-nowrap">
+                            <h1 className="font-bold">Product weight:</h1>
+                            {product.productItemWeight}
+                          </span>
+                          <span className=" flex flex-col text-nowrap">
+                            <h1 className="font-bold">Product unit:</h1>
+                            {product.productUnit}
+                          </span>
+                          <span className=" flex flex-col text-nowrap">
+                            <h1 className="font-bold">Product breath:</h1>
+                            {product.productBreath}
+                          </span>
+                          <span className=" flex flex-col text-nowrap">
+                            <h1 className="font-bold">Product length:</h1>
+                            {product.productLength}
+                          </span>
+                          <span className=" flex flex-col text-nowrap">
+                            <h1 className="font-bold">Product width:</h1>
+                            {product.productWidth}
+                          </span>
+                          {/* <span className="p-2 text-nowrap flex-1">
+                          {product.id}
+                        </span> */}
+                        </div>
+                      ))}
+                    </>
                   </div>
-                ))}
-              </>
-            )}
-          </div>
+                )}
+              </div>
+            </>
+          ))}
         </div>
       </div>
 
