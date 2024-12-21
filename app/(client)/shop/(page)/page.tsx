d1786e2ee500f5210b523fe32fcc7ebb5a8872c0
josephDev123 <josephuzuegbu55@gmail.com>
2024-12-21 18:25:31 +0100

    fixing bug

--- app/(client)/shop/(page)/page.tsx
+++ app/(client)/shop/(page)/page.tsx
@@ -27,7 +27,7 @@ export default async function page({
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_BASEURL}/api/product/products-paginate?page=${page}&limit=${limit}`,
     {
-      cache: "no-cache",
+      next: { revalidate: 600 },
     }
   );
 
@@ -70,7 +70,7 @@ export default async function page({
 
         <ItemLimit />
       </div>
-      <pre>{JSON.stringify(result)}</pre>
+
       <Suspense fallback={<Loading />}>
         <ProductListSection data={data} itemsNumber={totalDoc} />
       </Suspense>
