99c97c8c6ebda8984899ec642aab093d5b35dff4
josephDev123 <josephuzuegbu55@gmail.com>
2025-01-24 22:24:06 +0100

    testing continue

--- app/api/verify-order/route.ts
+++ app/api/verify-order/route.ts
@@ -37,7 +37,7 @@ export async function GET(req: NextRequest) {
     // ) {
     return NextResponse.json(
       {
-        message: "Order successful, please wait for confirmation",
+        message: "Order successful, please wait for confirmation ...",
         data: "hello",
       },
       { status: 200 }
