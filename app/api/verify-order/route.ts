bc8e8369a4e460d751e4d9733f6af3355a119021
josephDev123 <josephuzuegbu55@gmail.com>
2025-01-28 16:39:25 +0100

    finding bug

--- app/api/verify-order/route.ts
+++ app/api/verify-order/route.ts
@@ -7,40 +7,46 @@ import { startDb } from "@/lib/startDb";
 
 export const dynamic = "force-dynamic";
 export async function GET(req: NextRequest) {
-  // const request = req.nextUrl;
-  // const queryStatus = request.searchParams.get("status");
-  // const queryTx_ref = request.searchParams.get("tx_ref");
-  // const queryTransaction_id = request.searchParams.get("transaction_id");
+  const request = req.nextUrl;
+  const queryStatus = request.searchParams.get("status");
+  const queryTx_ref = request.searchParams.get("tx_ref");
+  const queryTransaction_id = request.searchParams.get("transaction_id");
   try {
-    // await startDb();
-    // const flw = new Flutterwave(
-    //   process.env.FLUTTERWAVE_PUBLIC_KEY,
-    //   process.env.FLUTTERWAVE_SECRET_KEY
-    // );
+    await startDb();
+    const flw = new Flutterwave(
+      process.env.FLUTTERWAVE_PUBLIC_KEY,
+      process.env.FLUTTERWAVE_SECRET_KEY
+    );
 
     // const queryParams = new URLSearchParams(request.searchParams);
 
     // console.log(queryStatus, queryTx_ref, queryTransaction_id);
-    // if (queryStatus === "successful") {
-    // const OrderDetails = await OrderModel.findOne({
-    //   tx_ref: queryTx_ref,
-    // });
-    // const response = await flw.Transaction.verify({
-    //   id: queryTransaction_id,
-    // });
+    if (queryStatus === "successful") {
+      const OrderDetails = await OrderModel.findOne({
+        tx_ref: queryTx_ref,
+      });
+      const response = await flw.Transaction.verify({
+        id: queryTransaction_id,
+      });
 
-    // console.log("orders", OrderDetails);
-    // console.log("response", response);
-    // if (
-    //   response.data.status === "successful" &&
-    //   // response.data.amount === OrderDetails.payment.amount &&
-    //   response.data.currency === "NGN"
-    // ) {
-    return NextResponse.json({ message: "success" }, { status: 200 });
-    // }
-    // } else {
-    //   return NextResponse.json({ message: "order failed" }, { status: 500 });
-    // }
+      // console.log("orders", OrderDetails);
+      // console.log("response", response);
+      if (
+        response.data.status === "successful" &&
+        response.data.amount === OrderDetails.payment.amount &&
+        response.data.currency === "NGN"
+      ) {
+        return NextResponse.json(
+          { message: "success", data: OrderDetails },
+          { status: 200 }
+        );
+      } else {
+        return NextResponse.json(
+          { message: "order has credibility issue" },
+          { status: 500 }
+        );
+      }
+    }
   } catch (error) {
     return NextResponse.json(
       { message: "something went wrong" },
