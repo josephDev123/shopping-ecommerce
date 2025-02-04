dd6b2c449902239d46b24e7703adf9370547aed2
josephDev123 <josephuzuegbu55@gmail.com>
2025-02-04 19:45:37 +0100

    fixing cont

--- app/(client)/order-outcome/page.tsx
+++ app/(client)/order-outcome/page.tsx
@@ -13,7 +13,7 @@ export default async function page({
   if (!response.ok) {
     return (
       <div className="h-42 flex flex-col justify-center items-center">
-        error occurs
+        error occur
       </div>
     );
   }
