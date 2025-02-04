9edc3b1d66d0eda4b3c7d47b463281cec0f3132e
josephDev123 <josephuzuegbu55@gmail.com>
2025-02-04 20:29:05 +0100

    fixing continues

--- app/api/verify-order/route.ts
+++ app/api/verify-order/route.ts
@@ -55,6 +55,6 @@ export async function GET(req: NextRequest) {
     // }
     // }
   } catch (error) {
-    return Response.json({ messages: error });
+    return Response.json({ message: error });
   }
 }
