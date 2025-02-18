aa5fe9f34db75dc24be39ac243f7bc0c31b2f378
josephDev123 <josephuzuegbu55@gmail.com>
2025-02-18 14:09:21 +0100

    fix

--- app/api/verify-order/route.ts
+++ app/api/verify-order/route.ts
@@ -8,21 +8,15 @@ export const dynamic = "force-dynamic";
 export async function GET(req: NextRequest) {
   try {
     // await startDb();
-    const searchParams = req.nextUrl.searchParams;
-    // const { searchParams } = new URL(req.url);
-    const queryStatus = searchParams.get("status");
-    const queryTx_ref = searchParams.get("tx_ref");
-    const queryTransaction_id = searchParams.get("transaction_id");
+    // const searchParams = req.nextUrl.searchParams;
+    // const queryStatus = searchParams.get("status");
+    // const queryTx_ref = searchParams.get("tx_ref");
+    // const queryTransaction_id = searchParams.get("transaction_id");
 
-    const flw = new Flutterwave(
-      process.env.FLUTTERWAVE_PUBLIC_KEY,
-      process.env.FLUTTERWAVE_SECRET_KEY
-    );
-
-    console.log(
-      process.env.FLUTTERWAVE_PUBLIC_KEY,
-      process.env.FLUTTERWAVE_SECRET_KEY
-    );
+    // const flw = new Flutterwave(
+    //   process.env.FLUTTERWAVE_PUBLIC_KEY,
+    //   process.env.FLUTTERWAVE_SECRET_KEY
+    // );
 
     // const OrderDetails = await OrderModel.findOne({
     //   tx_ref: queryTx_ref,
