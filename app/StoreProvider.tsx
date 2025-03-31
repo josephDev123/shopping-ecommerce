a84e0225442dbeb2adaf0059231737fd1b10e270
josephDev123 <josephuzuegbu55@gmail.com>
2025-03-31 20:40:44 +0100

    fix redux data persist

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -4,23 +4,25 @@ import { Provider } from "react-redux";
 import { AppStore, makeStore } from "../lib/store";
 import { useRef } from "react";
 import { PersistGate } from "redux-persist/integration/react";
-import { persistor } from "../lib/store";
+import { Persistor, persistStore } from "redux-persist";
 
 export default function StoreProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore>(makeStore());
+  const storeRef = useRef<AppStore>();
+  const persistorRef = useRef<Persistor>({} as Persistor);
   if (!storeRef.current) {
     storeRef.current = makeStore();
+    persistorRef.current = persistStore(storeRef.current);
   }
 
   return (
     <Provider store={storeRef.current}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      {children}
-      {/* </PersistGate> */}
+      <PersistGate loading={null} persistor={persistorRef.current}>
+        {children}
+      </PersistGate>
     </Provider>
   );
 }
--- lib/store.ts
+++ lib/store.ts
@@ -35,4 +35,4 @@ export const makeStore = () => {
 export type AppStore = ReturnType<typeof makeStore>;
 export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
-export let persistor = persistStore(makeStore());
+// export let persistor = persistStore(makeStore());
