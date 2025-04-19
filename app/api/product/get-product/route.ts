a12e8868733825aabcf9048749b32289fa130b69
josephDev123 <josephuzuegbu55@gmail.com>
2025-04-19 16:38:24 +0100

    fixing single product page

--- app/api/product/get-product/route.ts
+++ app/api/product/get-product/route.ts
@@ -6,37 +6,44 @@ import {
   ApiResponseHelper,
   SuccessApiResponseHelper,
 } from "../../utils/ApiResponseHelper";
-import { GlobalErrorHandlerType } from "@/app/utils/globarErrorHandler";
-import { NextRequest } from "next/server";
+import {
+  GlobalErrorHandler,
+  GlobalErrorHandlerType,
+} from "@/app/utils/globarErrorHandler";
+import { NextRequest, NextResponse } from "next/server";
+import GlobalError from "@/app/global-error";
 
 // export const dynamic = "force-dynamic";
-export async function GET(req: NextRequest) {
+export async function GET(req: NextRequest, res: NextResponse) {
   try {
-    await startDb();
-    const ProductRepositoryImp = new ProductRepository(ProductModel);
-    const ProductServiceImpl = new ProductService(ProductRepositoryImp);
-    const product_id = req.nextUrl.searchParams.get("product_id");
-    console.log(product_id);
-    const result = await ProductServiceImpl.findById(product_id!);
+    // await startDb();
+    // const ProductRepositoryImp = new ProductRepository(ProductModel);
+    // const ProductServiceImpl = new ProductService(ProductRepositoryImp);
+    // const product_id = req.nextUrl.searchParams.get("product_id");
     // console.log(product_id);
-    return SuccessApiResponseHelper(
-      result?.msg || "",
-      result?.name || "",
-      result?.operational || false,
-      result?.type || "",
-      result?.status || 200,
-      result?.data || {}
-    );
+    // const result = await ProductServiceImpl.findById(product_id || "");
+    // console.log(product_id);
+    // return SuccessApiResponseHelper(
+    //   result?.msg || "",
+    //   result?.name || "",
+    //   result?.operational || false,
+    //   result?.type || "",
+    //   result?.status || 200,
+    //   result?.data || {}
+    // );
+
+    return NextResponse.json({ msg: "hello" }, { status: 200 });
   } catch (error) {
     console.log(error);
-    const errorObj = error as GlobalErrorHandlerType;
-    // console.log(errorObj);
-    return ApiResponseHelper(
-      errorObj.msg,
-      errorObj.name,
-      errorObj.operational,
-      "error",
-      errorObj.code
-    );
+
+    if (error instanceof GlobalErrorHandler) {
+      return ApiResponseHelper(
+        error.msg,
+        error.name,
+        error.operational,
+        "error",
+        error.code as unknown as number
+      );
+    }
   }
 }
